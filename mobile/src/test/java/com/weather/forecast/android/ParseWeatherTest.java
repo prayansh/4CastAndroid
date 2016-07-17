3faa022a45f4bbd8e57521ba27a6eebc76223ad4
psn1997 <pavan.nichani.17@gmail.com>
2016-07-17 21:26:59 +0530

    Weather Test for Mumbai and Vancouver

--- mobile/src/test/java/com/weather/forecast/android/ParseWeatherTest.java
+++ mobile/src/test/java/com/weather/forecast/android/ParseWeatherTest.java
@@ -44,7 +44,30 @@ public class ParseWeatherTest {
     @Test
     public void testVancouver() {
         WeatherData data = JSONParsers.JSONWeatherParse(SampleData.VAN_WEATHER_DATA);
-        //TODO Pavan design test for vancouver weather data
-        //USe top for reference
+        assertEquals(287.2, data.getTemperature(), Math.pow(10, -3));
+        assertEquals(289.15, data.getTemperatureMax(), Math.pow(10, -3));
+        assertEquals(285.15, data.getTemperatureMin(), Math.pow(10, -3));
+
+        assertEquals(2.6, data.getWindSpeed(), Math.pow(10, -3));
+        assertEquals(60, data.getWindOrientation());
+
+        assertEquals(1016, data.getPressure(), Math.pow(10, -3));
+        assertEquals(1016, data.getGrndPressure(), Math.pow(10, -3));
+        assertEquals(1016, data.getSeaPressure(), Math.pow(10, -3));
+
+        assertEquals(72, data.getHumidity());
+        assertEquals(75, data.getCloudiness());
+
+        assertEquals(3.12, data.getRainVol());
+        assertEquals(-1, data.getSnowVol()); //Default Values
+        assertEquals(-1, data.getVisibility()); //Default Values
+
+        assertEquals(1468153155, data.getSunriseTime());
+        assertEquals(1468210572, data.getSunsetTime());
+        assertEquals(1468135606, data.getFetchTime());
+
+        assertTrue("light rain".equalsIgnoreCase(data.getWeatherDescription()));
+        assertEquals(WeatherType.RAIN, data.getWeatherType());
+        //TODO Prayansh check the above data set
     }
 }
